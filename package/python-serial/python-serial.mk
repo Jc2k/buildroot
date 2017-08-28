8ce940e4d224d69988417deea8ccc080f80382b3
John Carr <john.carr@unrouted.co.uk>
2017-08-28 12:35:16 +0100

    python-serial: Use distutils instead of setuptools

--- package/python-serial/python-serial.mk
+++ package/python-serial/python-serial.mk
@@ -9,7 +9,7 @@ PYTHON_SERIAL_SOURCE = pyserial-$(PYTHON_SERIAL_VERSION).tar.gz
 PYTHON_SERIAL_SITE = https://pypi.python.org/packages/ce/9c/694ce79a9d4a164e109aeba1a40fba23336f3b7554978553e22a5d41d54d
 PYTHON_SERIAL_LICENSE = BSD-3-Clause
 PYTHON_SERIAL_LICENSE_FILES = LICENSE.txt
-PYTHON_SERIAL_SETUP_TYPE = setuptools
+PYTHON_SERIAL_SETUP_TYPE = distutils
 
 # aio.py is an experimental module, that is compatible only with
 # Python 3, so remove it for Python 2 environment or it will
